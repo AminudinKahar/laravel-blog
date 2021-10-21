caa64859e916e87ef3f044a9d68a10c002ce34c9
AminudinKahar(GitHub) <aminudinkahar@gmail.com>
2021-10-21 12:21:58 +0800

    update individual seeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,24 +17,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $doe = User::factory()->newUser()->create();
-        
-        $else = User::factory(20)->create();
-
-        $users = $else->concat([$doe]);
+        $this->call([
+            UsersTableSeeder::class, 
+            BlogPostsTableSeeder::class, 
+            CommentssTableSeeder::class]);
 
         // test total users created in table
         // dd($users->count());
 
-        $posts = BlogPost::factory(50)->make()->each(function($post) use ($users){
-            $post->user_id = $users->random()->id;
-            $post->save();
-        });
+        
 
-        $comments = Comment::factory(150)->make()->each(function ($comment) use($posts) {
-            $comment->blog_post_id = $posts->random()->id;
-            $comment->save();
-        });
     }
     
 }
