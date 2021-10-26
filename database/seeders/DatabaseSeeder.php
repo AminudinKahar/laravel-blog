36c7325b854ce309cb652e7ac22e2cd5013b2826
AminudinKahar(GitHub) <aminudinkahar@gmail.com>
2021-10-26 12:37:58 +0800

    update delete method 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\BlogPost;
-use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        if ($this->command->confirm('Do you want to refresh the database?')) {
+            $this->command->call('migrate:refresh');
+            $this->command->info('Database was refreshed');
+        }
         // \App\Models\User::factory(10)->create();
         $this->call([
             UsersTableSeeder::class, 
             BlogPostsTableSeeder::class, 
-            CommentssTableSeeder::class]);
+            CommentssTableSeeder::class
+        ]);
 
         // test total users created in table
         // dd($users->count());
