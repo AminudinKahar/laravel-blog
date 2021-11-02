6af40eb1b8f9e5507c7d28a3213f290232c67339
AminudinKahar(GitHub) <aminudinkahar@gmail.com>
2021-11-02 16:43:02 +0800

    apply blade component feature in created and updated text

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,26 +2,39 @@
 
 @section('title', $post->title)
 @section('content')
-<h1>{{ $post->title }}</h1>
-<p>{{ $post->content }}</p>
-<p class="text-muted">
-    Added {{ $post->created_at->diffForHumans() }}
-    by {{ $post->user->name }}
-</p>
+    <h1>
+        {{ $post->title }}
 
-@if(now()->diffInMinutes($post->created_at) < 5)
-<div class="alert alert-info">New!</div>
-@endif
+        <x-badge type="primary" show="now()->diffInMinutes($post->created_at) < 30">
+            Brand new Post!
+        </x-badge>
+    </h1>
 
-<h4>Comments</h4>
+    <p>{{ $post->content }}</p>
+    {{--  <p class="text-muted">
+        Added {{ $post->created_at->diffForHumans() }}
+        by {{ $post->user->name }}
+    </p>  --}}
+
+    <x-updated date="{{ $post->created_at->diffForHumans() }}" name="{{ $post->user->name }}">
+    </x-updated>
+
+    <x-updated date="{{ $post->updated_at->diffForHumans() }}">
+        Updated
+    </x-updated>
+
+    <h4>Comments</h4>
 
 @forelse ($post->comments as $comment )
     <p>
         {{ $comment->content }}
     </p>
-    <p class="text-muted">
+    {{--  <p class="text-muted">
         Added {{ $comment->created_at->diffForHumans() }}
-    </p>
+    </p>  --}}
+    <x-updated date="{{ $comment->created_at->diffForHumans() }}">
+    </x-updated>
+
 @empty
     <p>No comments yet!</p>
 @endforelse
